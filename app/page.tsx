617a57c2f9c13b53076e2fb2d790a90a6a470e9a
Ruben Peeters <ruben@fampeeters.be>
2024-02-26 00:20:26 +0100

    Add trace beam home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,19 @@ import Footer from "@/components/custom/footer";
 import Hero from "@/components/custom/hero";
 import { Services } from "@/components/custom/services";
 import { Testimonials } from "@/components/custom/testimonies";
+import { TracingBeam } from "@/components/ui/tracing-beam";
 
 export default function Home() {
   return (
     <>
     <div className="flex flex-col items-center">
     <div className="max-w-7xl h-screen"><Hero></Hero></div>
-
+    <TracingBeam className="">
     <div className="max-w-screen-2xl pb-48"><Services></Services></div>
     <div className=" w-full"><Testimonials></Testimonials></div>
     <div className=" w-full py-48"><Contact></Contact></div>
     <div className=" w-full"><Footer></Footer></div>
-    
+    </TracingBeam>
     </div>
     </>
   );
