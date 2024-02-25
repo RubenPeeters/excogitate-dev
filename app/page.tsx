86f3538943e8e77ba77241e50d174fa4c6321e6f
Ruben Peeters <ruben@fampeeters.be>
2024-02-26 00:25:02 +0100

    Track beam to top

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <>
     <div className="flex flex-col items-center">
-    <div className="max-w-7xl h-screen"><Hero></Hero></div>
     <TracingBeam className="">
+    <div className="max-w-7xl h-screen"><Hero></Hero></div>
     <div className="max-w-screen-2xl pb-48"><Services></Services></div>
     <div className=" w-full"><Testimonials></Testimonials></div>
     <div className=" w-full py-48"><Contact></Contact></div>
