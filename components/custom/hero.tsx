2fda2ae18159d10a3fc05af94b0ac008a72bd190
Ruben Peeters <ruben@fampeeters.be>
2024-03-01 00:40:01 +0100

    Remove padding Hero

--- components/custom/hero.tsx
+++ components/custom/hero.tsx
@@ -7,7 +7,7 @@ import { BackgroundGradientAnimation } from "../ui/background-gradient-animation
 export default function Hero() {
   return (
     <BackgroundGradientAnimation>
-      <div className="h-screen w-full flex flex-col items-center justify-center overflow-hidden rounded-md py-32">
+      <div className="h-screen w-full flex flex-col items-center justify-center overflow-hidden rounded-md">
         <h1 className="text-7xl lg:text-9xl font-bold text-center text-white relative z-20">
           PragmiX
         </h1>
