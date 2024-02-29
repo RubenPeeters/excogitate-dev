12f57d878abfe280afaffc6bdd2039543837d103
Ruben Peeters <ruben@fampeeters.be>
2024-03-01 00:51:45 +0100

    Add navbar to hero, for styling purposes

--- components/custom/hero.tsx
+++ components/custom/hero.tsx
@@ -1,17 +1,20 @@
 "use client";
 import React from "react";
-import { SparklesCore } from "../ui/sparkles";
-import { useTheme } from "next-themes";
 import { BackgroundGradientAnimation } from "../ui/background-gradient-animation";
+import { Navbar } from "./navbar";
 
 export default function Hero() {
   return (
     <BackgroundGradientAnimation>
+      {/* Added Navbar here, so I could style it */}
+      <div className="flex justify-center p-4">
+        <Navbar></Navbar>
+      </div>
       <div className="h-screen w-full flex flex-col items-center justify-center overflow-hidden rounded-md">
         <h1 className="text-7xl lg:text-9xl font-bold text-center text-white relative z-20">
           PragmiX
         </h1>
-        <p className="mt-6 text-xl lg:text-2xl leading-8 text-gray-300 text-center max-w-5xl">
+        <p className="mt-6 text-xl lg:text-2xl leading-8 text-gray-300 text-center max-w-5xl m-6">
           Combining Data Mastery with Software Innovation, We engineer tailored
           solutions to meet your unique business challenges and goals.
         </p>
