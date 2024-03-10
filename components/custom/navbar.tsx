b89def28ccae185e567688ad2c1ae71b6d25e895
Ruben Peeters <ruben@fampeeters.be>
2024-03-10 22:39:51 +0100

    Remove ring around navbar

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -120,7 +120,7 @@ export default function Navbar() {
               leaveFrom="opacity-100 translate-y-0"
               leaveTo="opacity-0 -translate-y-1"
             >
-              <Popover.Panel className="absolute inset-x-0 top-0 -z-10 bg-white dark:bg-slate-950 pt-14 shadow-lg ring-1 ring-gray-900/5">
+              <Popover.Panel className="absolute inset-x-0 top-0 -z-10 bg-white dark:bg-slate-950 pt-14  ring-gray-900/5">
                 <div className="mx-auto grid max-w-7xl grid-cols-4 gap-x-4 px-6 py-10 lg:px-8 xl:gap-x-8">
                   {products.map((item) => (
                     <div
