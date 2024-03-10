f48fda9a96e878f22140d5e679bf731b5509b99e
Ruben Peeters <ruben@fampeeters.be>
2024-03-10 22:36:52 +0100

    Remove border contact

--- components/custom/contact.tsx
+++ components/custom/contact.tsx
@@ -5,7 +5,7 @@ export function Contact() {
   return (
     <div className="bg-gradient-to-tl dark:from-amber-800 dark:to-slate-950 to-white  from-amber-100">
       <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-        <div className="relative isolate overflow-hidden bg-white dark:bg-slate-950 px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16 border ">
+        <div className="relative isolate overflow-hidden bg-white dark:bg-slate-950 px-6 py-24 text-center sm:rounded-3xl sm:px-16 ">
           <h2 className="mx-auto max-w-2xl text-3xl font-bold tracking-tight dark:text-white sm:text-4xl">
             Let&apos;s work together!
           </h2>
@@ -16,7 +16,7 @@ export function Contact() {
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
               href="mailto:ruben@pragmix.io"
-              className="rounded-md bg-lime-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-lime-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-800"
+              className="rounded-md bg-lime-600 dark:bg-lime-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-800"
             >
               Reach out
             </a>
