9304288d00447bb8b87996b481f9c31bdb53f499
Ruben Peeters <ruben@fampeeters.be>
2024-03-23 10:14:12 +0100

    href in mobile nav

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -259,19 +259,19 @@ export default function Navbar() {
                   )}
                 </Disclosure>
                 <a
-                  href="#"
+                  href="/blog"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-gray-100 dark:hover:bg-gray-950"
                 >
                   Blog
                 </a>
                 <a
-                  href="#"
+                  href="/about"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-gray-100 dark:hover:bg-gray-950"
                 >
                   About
                 </a>
                 <a
-                  href="#"
+                  href="mailto:ruben@pragmix.io"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-gray-100 dark:hover:bg-gray-950"
                 >
                   Contact
